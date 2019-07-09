341d17b3e9c20b4829fb226e341972f3ee7a72c9
SustechJoy <11612917@mail.sustech.edu.cn>
2019-07-09 01:26:21 -0700

    finish before setters and getters

--- graph-viz/app/controllers/ForceBundling.java
+++ graph-viz/app/controllers/ForceBundling.java
@@ -1,13 +1,7 @@
 package controllers;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-class Node {
-    int id;
-    double x;
-    double y;
-}
 
 class Edge {
     int sourceNodeInd;
@@ -18,17 +12,24 @@ class Vector {
     double x;
     double y;
 
+    Vector() {
+    }
+
     Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
 }
 
+class Node extends Vector {
+    int id;
+}
+
 class Path {
-    ArrayList<Node> alv;
+    ArrayList<Vector> alv;
 
     Path() {
-        alv = new ArrayList<Node>();
+        alv = new ArrayList<>();
     }
 }
 
@@ -40,8 +41,8 @@ public class ForceBundling {
     static ArrayList<ArrayList<Integer>> compatibilityList = new ArrayList<>();
     final static double K = 0.1;
     final static double S_initial = 0.1;
-    final static double P_initial = 1;
-    final static double P_rate = 2;
+    final static int P_initial = 1;
+    final static int P_rate = 2;
     final static double C = 6;
     final static double I_initial = 90;
     final static double I_rate = 0.6666667;
@@ -66,7 +67,7 @@ public class ForceBundling {
     }
 
     // v1 for source, v2 for target
-    double customEdgeLength(Node v1, Node v2) {
+    double customEdgeLength(Vector v1, Vector v2) {
         return Math.sqrt(Math.pow(v1.x - v2.x, 2) + Math.pow(v1.y - v2.y, 2));
     }
 
@@ -76,7 +77,7 @@ public class ForceBundling {
         return new Vector(midX, midY);
     }
 
-    double euclideanDistance(Node p, Node q) {
+    double euclideanDistance(Vector p, Vector q) {
         return Math.sqrt(Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2));
     }
 
@@ -128,9 +129,9 @@ public class ForceBundling {
     }
 
     Vector applySpringForce(int e_ind, int i, double kP) {
-        Node prev = subdivisionPoints.get(e_ind).alv.get(i - 1);
-        Node next = subdivisionPoints.get(e_ind).alv.get(i + 1);
-        Node crnt = subdivisionPoints.get(e_ind).alv.get(i);
+        Vector prev = subdivisionPoints.get(e_ind).alv.get(i - 1);
+        Vector next = subdivisionPoints.get(e_ind).alv.get(i + 1);
+        Vector crnt = subdivisionPoints.get(e_ind).alv.get(i);
         double x = prev.x - crnt.x + next.x - crnt.x;
         double y = prev.y - crnt.y + next.y - crnt.y;
         x *= kP;
@@ -146,8 +147,8 @@ public class ForceBundling {
             double y = subdivisionPoints.get(compatibleEdgeList.get(oe)).alv.get(i).y - subdivisionPoints.get(e_ind).alv.get(i).y;
             Vector force = new Vector(x, y);
             if ((Math.abs(force.x) > eps || Math.abs(force.y) > eps)) {
-                Node source = subdivisionPoints.get(compatibleEdgeList.get(oe)).alv.get(i);
-                Node target = subdivisionPoints.get(e_ind).alv.get(i);
+                Vector source = subdivisionPoints.get(compatibleEdgeList.get(oe)).alv.get(i);
+                Vector target = subdivisionPoints.get(e_ind).alv.get(i);
                 double diff = (1 / Math.pow(customEdgeLength(source, target), 1));
                 sumOfForces.x += force.x * diff;
                 sumOfForces.y += force.y * diff;
@@ -156,7 +157,7 @@ public class ForceBundling {
         return sumOfForces;
     }
 
-    ArrayList<Vector> applyResultingForcesOnSubdivisionPoints(int e_ind, double P, double S) {
+    ArrayList<Vector> applyResultingForcesOnSubdivisionPoints(int e_ind, int P, double S) {
         double kP = K / (edgeLength(dataEdges.get(e_ind)) * (P + 1));
         ArrayList<Vector> resultingForcesForSubdivisionPoints = new ArrayList<>();
         resultingForcesForSubdivisionPoints.add(new Vector(0, 0));
@@ -171,5 +172,127 @@ public class ForceBundling {
         resultingForcesForSubdivisionPoints.add(new Vector(0, 0));
         return resultingForcesForSubdivisionPoints;
     }
+
+    void updateEdgeDivisions(int P) {
+        for (int e_ind = 0; e_ind < dataEdges.size(); e_ind++) {
+            if (P == 1) {
+                subdivisionPoints.get(e_ind).alv.add(dataNodes.get(dataEdges.get(e_ind).sourceNodeInd));
+                subdivisionPoints.get(e_ind).alv.add(edgeMidPoint(dataEdges.get(e_ind)));
+                subdivisionPoints.get(e_ind).alv.add(dataNodes.get(dataEdges.get(e_ind).targetNodeInd));
+            } else {
+                double dividedEdgeLength = computeDividedEdgeLength(e_ind);
+                double segmentLength = dividedEdgeLength / (P + 1);
+                double currentSegmentLength = segmentLength;
+                ArrayList<Vector> newDivisionPoints = new ArrayList<>();
+                newDivisionPoints.add(dataNodes.get(dataEdges.get(e_ind).sourceNodeInd));
+                for (int i = 1; i < subdivisionPoints.get(e_ind).alv.size(); i++) {
+                    double oldSegmentLength = euclideanDistance(subdivisionPoints.get(e_ind).alv.get(i), subdivisionPoints.get(e_ind).alv.get(i - 1));
+                    while (oldSegmentLength > currentSegmentLength) {
+                        double percentPosition = currentSegmentLength / oldSegmentLength;
+                        double newDivisionPointX = subdivisionPoints.get(e_ind).alv.get(i - 1).x;
+                        double newDivisionPointY = subdivisionPoints.get(e_ind).alv.get(i - 1).y;
+                        newDivisionPointX += percentPosition * (subdivisionPoints.get(e_ind).alv.get(i).x - subdivisionPoints.get(e_ind).alv.get(i - 1).x);
+                        newDivisionPointY += percentPosition * (subdivisionPoints.get(e_ind).alv.get(i).y - subdivisionPoints.get(e_ind).alv.get(i - 1).y);
+                        newDivisionPoints.add(new Vector(newDivisionPointX, newDivisionPointY));
+                        oldSegmentLength -= currentSegmentLength;
+                        currentSegmentLength = segmentLength;
+                    }
+                    currentSegmentLength -= oldSegmentLength;
+                }
+                newDivisionPoints.add(dataNodes.get(dataEdges.get(e_ind).targetNodeInd));
+                subdivisionPoints.get(e_ind).alv = newDivisionPoints;
+            }
+        }
+    }
+
+    double angleCompatibility(Edge P, Edge Q) {
+        return Math.abs(vectorDotProduct(edgeAsVector(P), edgeAsVector(Q)) / (edgeLength(P) * edgeLength(Q)));
+    }
+
+    double scaleCompatibility(Edge P, Edge Q) {
+        double lavg = (edgeLength(P) + edgeLength(Q)) / 2.0;
+        return 2.0 / (lavg / Math.min(edgeLength(P), edgeLength(Q)) + Math.max(edgeLength(P), edgeLength(Q)) / lavg);
+    }
+
+    double positionCompatibility(Edge P, Edge Q) {
+        double lavg = (edgeLength(P) + edgeLength(Q)) / 2.0;
+        Vector midP = new Vector(
+                (dataNodes.get(P.sourceNodeInd).x + dataNodes.get(P.targetNodeInd).x) / 2.0,
+                (dataNodes.get(P.sourceNodeInd).y + dataNodes.get(P.targetNodeInd).y) / 2.0
+        );
+        Vector midQ = new Vector(
+                (dataNodes.get(Q.sourceNodeInd).x + dataNodes.get(Q.targetNodeInd).x) / 2.0,
+                (dataNodes.get(Q.sourceNodeInd).y + dataNodes.get(Q.targetNodeInd).y) / 2.0
+        );
+        return lavg / (lavg + euclideanDistance(midP, midQ));
+    }
+
+    double edgeVisibility(Edge P, Edge Q) {
+        Vector I0 = projectPointOnLine(dataNodes.get(Q.sourceNodeInd), dataNodes.get(P.sourceNodeInd), dataNodes.get(P.targetNodeInd));
+        Vector I1 = projectPointOnLine(dataNodes.get(Q.targetNodeInd), dataNodes.get(P.sourceNodeInd), dataNodes.get(P.targetNodeInd));
+        Vector midI = new Vector(
+                (I0.x + I1.x) / 2.0,
+                (I0.y + I1.y) / 2.0
+        );
+        Vector midP = new Vector(
+                (dataNodes.get(P.sourceNodeInd).x + dataNodes.get(P.targetNodeInd).x) / 2.0,
+                (dataNodes.get(P.sourceNodeInd).y + dataNodes.get(P.targetNodeInd).y) / 2.0
+        );
+        return Math.max(0, 1 - 2 * euclideanDistance(midP, midI) / euclideanDistance(I0, I1));
+    }
+
+    double visibilityCompatibility(Edge P, Edge Q) {
+        return Math.min(edgeVisibility(P, Q), edgeVisibility(Q, P));
+    }
+
+    double compatibilityScore(Edge P, Edge Q) {
+        return (angleCompatibility(P, Q) * scaleCompatibility(P, Q) * positionCompatibility(P, Q) * visibilityCompatibility(P, Q));
+    }
+
+    boolean areCompatible(Edge P, Edge Q) {
+        return (compatibilityScore(P, Q) > compatibility_threshold);
+    }
+
+    void computeCompatibilityLists() {
+        for (int e = 0; e < dataEdges.size() -1 ; e++) {
+            for(int oe = e + 1; oe < dataEdges.size(); oe++){
+                if(areCompatible(dataEdges.get(e), dataEdges.get(oe))){
+                    compatibilityList.get(e).add(oe);
+                    compatibilityList.get(oe).add(e);
+                }
+            }
+        }
+    }
+
+    ArrayList<Path> forceBundle() {
+        double S = S_initial;
+        double I = I_initial;
+        int P = P_initial;
+        initializeEdgeSubdivisions();
+        initializeCompatibilityLists();
+        updateEdgeDivisions(P);
+        computeCompatibilityLists();
+        for(int cycle = 0; cycle < C; cycle ++) {
+            for(int iteration = 0; iteration < I; iteration++) {
+                ArrayList<ArrayList<Vector>> forces = new ArrayList<>();
+                for(int edge = 0; edge < dataEdges.size(); edge++){
+                    forces.set(edge, applyResultingForcesOnSubdivisionPoints(edge, P, S));
+                }
+                for(int e = 0; e < dataEdges.size(); e++){
+                    for(int i=0;i<P+1;i++) {
+                        subdivisionPoints.get(e).alv.get(i).x += forces.get(e).get(i).x;
+                        subdivisionPoints.get(e).alv.get(i).y += forces.get(e).get(i).y;
+                    }
+                }
+            }
+            S = S / 2;
+            P = P * P_rate;
+            I = I_rate * I;
+            updateEdgeDivisions(P);
+        }
+        return subdivisionPoints;
+    }
+    
+    // TODO: setters and getters
 
 }
